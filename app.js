a47d941cea1543772ab9038e86d32449c512ea3e
Jatinder Singh <jsbirdi0001@gmail.com>
2019-01-10 20:19:31 +0000

    Created app.js

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
 var express = require("express"),
-    app = express();
+    app = express(),
+    bodyParser = require("body-parser"),
+    mongoose = require("mongoose");
+
+    
+    
+    
+app.use(express.static("public"));
+app.set("view engine","ejs");
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/",function(req,res){
+    res.render("index")
+})
+    
+app.listen(process.env.IP, process.env.PORT, function(){
+    console.log("Informa App has been started")
+})
     
