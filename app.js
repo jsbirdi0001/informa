3c925c84af0c168c42c18dac078cfbb3b05aca84
Jatinder Singh <jsbirdi0001@gmail.com>
2019-01-10 20:40:43 +0000

    JJ

--- app.js
+++ app.js
@@ -1,20 +1,18 @@
 var express = require("express"),
     app = express(),
     bodyParser = require("body-parser"),
-    mongoose = require("mongoose");
-
-    
-    
+    mongoose = require("mongoose"),
+    methodOverride = require("method-override");
     
 app.use(express.static("public"));
-app.set("view engine","ejs");
 app.use(bodyParser.urlencoded({extended:true}));
+app.set("view engine","ejs");
+app.use(methodOverride("_method"));
 
 app.get("/",function(req,res){
-    res.render("index")
-})
+    res.render("index");
+});
     
-app.listen(process.env.IP, process.env.PORT, function(){
-    console.log("Informa App has been started")
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("Informa app has been Started");
 })
-    
